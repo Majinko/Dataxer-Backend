6b1143febf8b313a7935a25bf5c4c5366dac9a3b
Milan Pizem <milan.pizem@gmail.com>
2020-09-02 23:19:49 +0200

    fix: re-roll security utils method

--- src/main/java/com/data/dataxer/securityContextUtils/SecurityUtils.java
+++ src/main/java/com/data/dataxer/securityContextUtils/SecurityUtils.java
@@ -27,12 +27,6 @@ public class SecurityUtils {
     }
 
     public static Company defaultCompany() {
-        Company defCompany = null;
-        List<Company> companies = ((FirebaseUserAuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getCompanies();
-        if (!companies.isEmpty()) {
-            return companies.get(0);
-        }
-        //return ((FirebaseUserAuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getCompanies().get(0);
-        return null;
+        return ((FirebaseUserAuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getCompanies().get(0);
     }
 }
