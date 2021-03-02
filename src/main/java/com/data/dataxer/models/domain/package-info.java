537cbc4b4ea7085241cd796fa9cacc1d264ff14b
MilanPizem <milan.pizem@gmail.com>
2021-03-02 21:55:29 +0100

    chore: revert category rework 2

--- src/main/java/com/data/dataxer/models/domain/package-info.java
+++
@@ -1,6 +0,0 @@
-@QueryEntities(value = {MpttEntity.class, TreeEntity.class})
-package com.data.dataxer.models.domain;
-
-import com.querydsl.core.annotations.QueryEntities;
-import works.hacker.mptt.TreeEntity;
-import works.hacker.mptt.classic.MpttEntity;
