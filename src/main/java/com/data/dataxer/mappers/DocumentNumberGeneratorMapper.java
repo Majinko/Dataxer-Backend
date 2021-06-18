97b182b368bb9e64e35fd786cdbf2ed43658667d
Marek Hlavco <hlavco@dataid.sk>
2021-06-18 10:48:31 +0200

    changes properties

--- src/main/java/com/data/dataxer/mappers/DocumentNumberGeneratorMapper.java
+++ src/main/java/com/data/dataxer/mappers/DocumentNumberGeneratorMapper.java
@@ -6,11 +6,9 @@ import org.mapstruct.Mapper;
 
 @Mapper
 public interface DocumentNumberGeneratorMapper {
-
     DocumentNumberGenerator documentNumberGeneratorDTOToDocumentNumberGenerator(DocumentNumberGeneratorDTO documentNumberGeneratorDTO);
 
     DocumentNumberGeneratorDTO documentNumberGeneratorToDocumentNumberGeneratorDTO(DocumentNumberGenerator documentNumberGenerator);
 
     DocumentNumberGeneratorDTO documentNumberGeneratorToDocumentNumberGeneratorDTOSimple(DocumentNumberGenerator documentNumberGenerator);
-
 }
