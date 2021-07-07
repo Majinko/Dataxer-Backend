c5528dafaf36616bf8ec54ebf0e15294471781a4
MilanPizem <milan.pizem@gmail.com>
2021-07-07 22:54:43 +0200

    fix: premenovanie pre konzistentnost

--- src/main/java/com/data/dataxer/services/BillingInformationService.java
+++ src/main/java/com/data/dataxer/services/BillingInformationService.java
@@ -2,28 +2,11 @@ package com.data.dataxer.services;
 
 import com.data.dataxer.models.domain.BillingInformation;
 import com.data.dataxer.models.domain.Company;
-import com.data.dataxer.repositories.BillingInformationRepository;
 
 import java.util.List;
 
-public class BillingInformationService implements BillingInformationServiceImpl{
-    private final BillingInformationRepository billingInformationRepository;
+public interface BillingInformationService {
+    void store(Company company, List<BillingInformation> billingInformation);
 
-    public BillingInformationService(BillingInformationRepository billingInformationRepository) {
-        this.billingInformationRepository = billingInformationRepository;
-    }
-
-    @Override
-    public void store(Company company, List<BillingInformation> billingInformation) {
-        billingInformation.forEach(b -> {
-            b.setCompany(company);
-
-            this.billingInformationRepository.save(b);
-        });
-    }
-
-    @Override
-    public void update(List<BillingInformation> billingInformation) {
-
-    }
+    void update(List<BillingInformation> billingInformation);
 }
--- src/main/java/com/data/dataxer/services/BillingInformationServiceImpl.java
+++ src/main/java/com/data/dataxer/services/BillingInformationServiceImpl.java
@@ -2,11 +2,28 @@ package com.data.dataxer.services;
 
 import com.data.dataxer.models.domain.BillingInformation;
 import com.data.dataxer.models.domain.Company;
+import com.data.dataxer.repositories.BillingInformationRepository;
 
 import java.util.List;
 
-public interface BillingInformationServiceImpl {
-    void store(Company company, List<BillingInformation> billingInformation);
+public class BillingInformationServiceImpl implements BillingInformationService {
+    private final BillingInformationRepository billingInformationRepository;
 
-    void update(List<BillingInformation> billingInformation);
+    public BillingInformationServiceImpl(BillingInformationRepository billingInformationRepository) {
+        this.billingInformationRepository = billingInformationRepository;
+    }
+
+    @Override
+    public void store(Company company, List<BillingInformation> billingInformation) {
+        billingInformation.forEach(b -> {
+            b.setCompany(company);
+
+            this.billingInformationRepository.save(b);
+        });
+    }
+
+    @Override
+    public void update(List<BillingInformation> billingInformation) {
+
+    }
 }
